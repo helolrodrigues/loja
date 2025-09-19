737758168050d389ed7914bd370b0d98521d9404
helolrodrigues <heloisa.rodrigues19@etec.sp.gov.br>
2025-09-19 16:51:38 -0300

    atualização

--- index.php
+++ index.php
@@ -3,13 +3,37 @@
 ?>
 <body>
     <div class="container">
-        <h1>Bem vindo ao 1° Sistema usando o CRUD</h1>
-        <h2>Heloísa Lima Rodrigues</h2>
+        <h1>CREPE MANIA</h1>
+        <h2>Seu novo lugar favorito para crepes!</h2>
 
         <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-            <a href = "form_cadastrar.php"type="button" class="btn btn-danger">Cadastrar</a>
-            <a href = "listar.php"type="button" class="btn btn-warning">Listar</a>
+            <a href = "form_cadastrar.php"type="button" class="btn btn-outline-secondary">Cadastrar</a>
+            <a href = "listar.php"type="button" class="btn btn-outline-secondary">Listar</a>
         </div>
+
+        <style>
+            h1{
+                color: #D84B16;
+            }
+
+            h2{
+                color:#D2b48c;
+            }
+        html, body {
+    height: 100%; 
+    display: flex; 
+    justify-content: center; 
+    align-items: center; 
+    text-align: center; 
+    font-family: Montserrat;
+    background-color: #FFF5E1ed;
+}
+
+    btn-group{
+        font-family: Arial;
+    }
+
+        </style>
 
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous">
